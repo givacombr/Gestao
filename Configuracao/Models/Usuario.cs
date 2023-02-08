df3fa046f7e27e19062852e2e40e23555a80eeaf
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-07 22:02:10 -0300

    Criando novo banco de dados
    vamos começar a usar o sql server

---
+++ Configuracao/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+    public class Usuario
+    {
+        public string? Nome { get; set; }
+        public string? NomeUsuario { get; set; }
+        public string? CPF { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public bool Ativo { get; set; }
+        public List<GrupoUsuario> GrupoUsuarios { get; set; }
+    }
+}
