e4b66caec999d0dbdc0ac6653a2113245d1e04a2
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-09 21:55:49 -0300

    Inicio da implementação da classe Usuario
    Paramos no inicio do metodo inserir

---
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();//cn é um objeto de conexao
+
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Usuario Buscar(string _nomeUsuario)
+        {
+            return new Usuario();//para solucionar o erro de vazio.
+        }
+        public void Alterar(Usuario usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}
