aff29535cc11cab2787d852bae58e37b4f381a90
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-10 21:53:26 -0300

    atualizando 10/02/23
    Atualizando o banco

--- Configuracao/DAL/UsuarioDAL.cs
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -14,11 +14,24 @@ namespace DAL
             {
                 cn.ConnectionString = Conexao.StringDeConexao;
                 SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText =   @"INSERT INTO Usuario(Nome, NomeUsuario, CPF, Email, Senha, Ativo)
+                                      VALUES (@Nome, @NomeUsuario, @CPF, @Email, @Senha, @Ativo)";
 
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", _usuario.Nome);
+                cmd.Parameters.AddWithValue("@NomeUsuario", _usuario.NomeUsuario);
+                cmd.Parameters.AddWithValue("@CPF", _usuario.CPF);
+                cmd.Parameters.AddWithValue("@Email", _usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", _usuario.Senha);
+                cmd.Parameters.AddWithValue("@Ativo", _usuario.Ativo);
+
+                cn.Open();
+                cmd.ExecuteScalar();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
+                throw new Exception("Ocorreu um erro ao tentar inserir um usuário no banco: " + ex.Message);
 
             }
             finally
