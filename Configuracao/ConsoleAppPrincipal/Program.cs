f1fb616f3b8d775075bfb0e142538531282dd27c
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-14 21:43:14 -0300

    Realizando teste
    ok

--- Configuracao/ConsoleAppPrincipal/Program.cs
+++ Configuracao/ConsoleAppPrincipal/Program.cs
@@ -7,15 +7,25 @@ namespace ConsoleAppPrincipal
     {
         private static void Main(string[] args)
         {
-            UsuarioBLL usuarioBLL = new UsuarioBLL();
-            Usuario usuario = new Usuario();
-            usuario.Nome = "Erisvaldo Carvalho Silva";
-            usuario.NomeUsuario = "erisvaldo";
-            usuario.Ativo = true;
-            usuario.Email = "contato@erisvaldocarvalho.com.br";
-            usuario.CPF = "458.158.442-88";
-            usuario.Senha = "123";
-            usuarioBLL.Inserir(usuario);
+            try
+            {
+                Usuario usuario = new Usuario();
+                UsuarioBLL usuarioBLL = new UsuarioBLL();
+
+                usuario.Nome = "Erisvaldo Carvalho Silva";
+                usuario.NomeUsuario = "erisvaldo";
+                usuario.Ativo = true;
+                usuario.Email = "contato@erisvaldocarvalho.com.br";
+                usuario.CPF = "458.158.442-88";
+                usuario.Senha = "504587545";
+
+                usuarioBLL.Inserir(usuario);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
