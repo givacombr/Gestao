771d421b4d4f90a6df3c28357b11c024c1982eb9
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-27 20:35:55 -0300

    Testando27022023

--- Configuracao/DAL/Conexao.cs
+++ Configuracao/DAL/Conexao.cs
@@ -6,8 +6,8 @@
         {
             get
             {
-                /*return @"User ID=SA;Initial Catalog=Gestao;Data Source=.\SQLEXPRESS2019;Password=Senailab02";*/
-                return @"User ID=SA;Initial Catalog=Gestao;Data Source=.\SQLEXPRESS;Password=admin123";
+                return @"User ID=SA;Initial Catalog=Gestao;Data Source=.\SQLEXPRESS2019;Password=Senailab02";
+                /*return @"User ID=SA;Initial Catalog=Gestao;Data Source=.\SQLEXPRESS;Password=admin123";*/
             }
         }
     }
---
+++ Revisao/Revisao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
