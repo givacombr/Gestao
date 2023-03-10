c86be12336abf483f8faf9397fabc4a67d170bfa
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-03-09 21:48:40 -0300

    atualizando o banco de dados090323

---
+++ Configuracao/Banco/SQLQuery6_09032023.sql
@@ -0,0 +1,10 @@
+select * from Usuario
+SELECT * FROM GrupoUsuario
+select * from UsuarioGrupoUsuario
+
+insert into UsuarioGrupoUsuario values(1,2)
+insert into UsuarioGrupoUsuario values(1,3)
+insert into UsuarioGrupoUsuario values(3,1)
+insert into UsuarioGrupoUsuario values(3,3)
+insert into UsuarioGrupoUsuario values(12,2)
+insert into UsuarioGrupoUsuario values(12,1)
