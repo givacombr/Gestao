76ba7b705114a638006407f82a54d9f4e8da2405
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-06-09 21:58:32 -0300

    Criação do Form Consulta e Cadastro de cliente

---
+++ Professor/Configuracao/WindowsFormsAppPrincipal/FormCadastroCliente.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsAppPrincipal
+{
+    partial class FormCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label nomeLabel;
+            System.Windows.Forms.Label cPFLabel;
+            System.Windows.Forms.Label rGLabel;
+            System.Windows.Forms.Label emailLabel;
+            System.Windows.Forms.Label foneLabel;
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.clienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cPFTextBox = new System.Windows.Forms.TextBox();
+            this.rGTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.foneTextBox = new System.Windows.Forms.TextBox();
+            this.labelCadastro = new System.Windows.Forms.Label();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            nomeLabel = new System.Windows.Forms.Label();
+            cPFLabel = new System.Windows.Forms.Label();
+            rGLabel = new System.Windows.Forms.Label();
+            emailLabel = new System.Windows.Forms.Label();
+            foneLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nomeLabel
+            // 
+            nomeLabel.AutoSize = true;
+            nomeLabel.Location = new System.Drawing.Point(2, 82);
+            nomeLabel.Name = "nomeLabel";
+            nomeLabel.Size = new System.Drawing.Size(44, 16);
+            nomeLabel.TabIndex = 1;
+            nomeLabel.Text = "Nome";
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.clienteBindingSource, "Nome", true));
+            this.nomeTextBox.Location = new System.Drawing.Point(5, 101);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(445, 22);
+            this.nomeTextBox.TabIndex = 2;
+            // 
+            // clienteBindingSource
+            // 
+            this.clienteBindingSource.DataSource = typeof(Models.Cliente);
+            // 
+            // cPFLabel
+            // 
+            cPFLabel.AutoSize = true;
+            cPFLabel.Location = new System.Drawing.Point(453, 82);
+            cPFLabel.Name = "cPFLabel";
+            cPFLabel.Size = new System.Drawing.Size(33, 16);
+            cPFLabel.TabIndex = 3;
+            cPFLabel.Text = "CPF";
+            // 
+            // cPFTextBox
+            // 
+            this.cPFTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.clienteBindingSource, "CPF", true));
+            this.cPFTextBox.Location = new System.Drawing.Point(456, 101);
+            this.cPFTextBox.Name = "cPFTextBox";
+            this.cPFTextBox.Size = new System.Drawing.Size(100, 22);
+            this.cPFTextBox.TabIndex = 4;
+            // 
+            // rGLabel
+            // 
+            rGLabel.AutoSize = true;
+            rGLabel.Location = new System.Drawing.Point(568, 82);
+            rGLabel.Name = "rGLabel";
+            rGLabel.Size = new System.Drawing.Size(27, 16);
+            rGLabel.TabIndex = 5;
+            rGLabel.Text = "RG";
+            // 
+            // rGTextBox
+            // 
+            this.rGTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.clienteBindingSource, "RG", true));
+            this.rGTextBox.Location = new System.Drawing.Point(562, 101);
+            this.rGTextBox.Name = "rGTextBox";
+            this.rGTextBox.Size = new System.Drawing.Size(100, 22);
+            this.rGTextBox.TabIndex = 6;
+            // 
+            // emailLabel
+            // 
+            emailLabel.AutoSize = true;
+            emailLabel.Location = new System.Drawing.Point(111, 140);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new System.Drawing.Size(45, 16);
+            emailLabel.TabIndex = 7;
+            emailLabel.Text = "E-mail";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.clienteBindingSource, "Email", true));
+            this.emailTextBox.Location = new System.Drawing.Point(111, 159);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(339, 22);
+            this.emailTextBox.TabIndex = 8;
+            // 
+            // foneLabel
+            // 
+            foneLabel.AutoSize = true;
+            foneLabel.Location = new System.Drawing.Point(2, 140);
+            foneLabel.Name = "foneLabel";
+            foneLabel.Size = new System.Drawing.Size(38, 16);
+            foneLabel.TabIndex = 9;
+            foneLabel.Text = "Fone";
+            // 
+            // foneTextBox
+            // 
+            this.foneTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.clienteBindingSource, "Fone", true));
+            this.foneTextBox.Location = new System.Drawing.Point(5, 159);
+            this.foneTextBox.Name = "foneTextBox";
+            this.foneTextBox.Size = new System.Drawing.Size(100, 22);
+            this.foneTextBox.TabIndex = 10;
+            // 
+            // labelCadastro
+            // 
+            this.labelCadastro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCadastro.Location = new System.Drawing.Point(0, 0);
+            this.labelCadastro.Name = "labelCadastro";
+            this.labelCadastro.Size = new System.Drawing.Size(675, 47);
+            this.labelCadastro.TabIndex = 11;
+            this.labelCadastro.Text = "Cadastro de Cliente";
+            this.labelCadastro.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(509, 335);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.TabIndex = 12;
+            this.buttonSalvar.Text = "&Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancelar.Location = new System.Drawing.Point(590, 335);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 13;
+            this.buttonCancelar.Text = "&Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FormCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 372);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.labelCadastro);
+            this.Controls.Add(foneLabel);
+            this.Controls.Add(this.foneTextBox);
+            this.Controls.Add(emailLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(rGLabel);
+            this.Controls.Add(this.rGTextBox);
+            this.Controls.Add(cPFLabel);
+            this.Controls.Add(this.cPFTextBox);
+            this.Controls.Add(nomeLabel);
+            this.Controls.Add(this.nomeTextBox);
+            this.Name = "FormCadastroCliente";
+            this.Text = "FormCadastroCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource clienteBindingSource;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.TextBox cPFTextBox;
+        private System.Windows.Forms.TextBox rGTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox foneTextBox;
+        private System.Windows.Forms.Label labelCadastro;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonCancelar;
+    }
+}
