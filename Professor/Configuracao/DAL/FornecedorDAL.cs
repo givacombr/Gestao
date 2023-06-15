6e2443a28543ff64de22504d51ad736ec21ab3c9
Giv@s <giva.com.br@gmail.com>
2023-06-15 07:47:27 -0300

    Criação do Método Inserir na classe Fornecedor

---
+++ Professor/Configuracao/DAL/FornecedorDAL.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class FornecedorDAL
+    {
+        public void Inserir(Fornecedor _fornecedor)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Fornecedor(Nome, Fone, Email, Site) VALUES (@Nome, @Fone, @Email, @Site)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", _fornecedor.Nome);
+                cmd.Parameters.AddWithValue("@Fone", _fornecedor.Fone);
+                cmd.Parameters.AddWithValue("@Email", _fornecedor.Email);
+                cmd.Parameters.AddWithValue("@Site", _fornecedor.Site);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um fornecedor no banco de dados.", ex) { Data = { { "Id", 0 } } };
+            }
+            finally 
+            { 
+                cn.Close(); 
+            }
+        }
+    }
+}
