fd0d3b0858e5665e1336259018d1a82d939b1cc6
Giv@s <giva.com.br@gmail.com>
2023-09-22 08:12:48 -0300

    nao sei do que se trata

--- Professor/Configuracao/WindowsFormsAppPrincipal/FormPrincipal.Designer.cs
+++ Professor/Configuracao/WindowsFormsAppPrincipal/FormPrincipal.Designer.cs
@@ -103,7 +103,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(658, 423);
             this.MinimizeBox = false;
